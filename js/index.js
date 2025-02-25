fa9f5a5719566ae70d69f5ffd20e27f42d78ab01
Dang Thai Huy <68779552+dangthaihuy2001@users.noreply.github.com>
2025-02-25 23:34:55 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -24,7 +24,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate('|#countdown 3||Anh|Yêu|Nhàn|❤|Yêu bé|');
+      S.UI.simulate('|#countdown 3||RẢNH|LẮM|HẢ|MÁ|VÃI THẬT|');
     }
 
     S.Drawing.loop(function () {
